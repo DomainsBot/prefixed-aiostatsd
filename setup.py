8f13719cb91dcdff79c81d85ab05c09e4557f568
Javier Ruere <javier@ruere.com.ar>
2022-04-16 22:27:01 +0300

    Bump patch version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def load_requirements(file_name: str) -> Iterator[str]:
 
 setup(
     name="prefixed-aiostatsd",
-    version="0.2",
+    version="0.3",
     description="Prefixed AioStatsD",
     classifiers=[
         "License :: Other/Proprietary License",
